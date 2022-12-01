93567fdd6acf0a05b798f76a3560ec60f82a690a
Gamaliel Abanto <gabanto@canvia.com>
2022-12-01 12:45:21 -0500

    descargar pdf

---
+++ src/app/helpers/services/download-document.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import jsPDF from 'jspdf'
+import autoTable from 'jspdf-autotable'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DownloadDocumentService {
+
+  constructor() { }
+
+  public exportPdf(columns:any[], data: any[]) {
+    const doc = new jsPDF()
+    autoTable(doc, { columns, body: data });
+    doc.save('table.pdf');
+  }
+}
--- src/app/pages/operador-app/operador-app.component.ts
+++ src/app/pages/operador-app/operador-app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { DownloadDocumentService } from '@helpers/services/download-document.service';
 
 @Component({
   selector: 'app-operador-app',
@@ -9,12 +11,46 @@ export class OperadorAppComponent {
   data: any[] = DATA;
   first = 0;
   date: Date;
-  constructor() { }
+  itemsDownload: MenuItem[];
+  cols: any[] = [
+    { field: 'Fecha', header: 'fecha' },
+    { field: 'Monto', header: 'monto' },
+    { field: 'Placa', header: 'placa' },
+    { field: 'Empresa', header: 'fecha' },
+    { field: 'Ruta', header: 'ruta' },
+    { field: 'Ubicacion', header: 'ubicacion' },
+    { field: 'Origen', header: 'origen' }
+  ];
+  exportColumns: any[];
+  constructor(
+    private _downloadDocumentService: DownloadDocumentService
+  ) { }
 
   ngOnInit(): void {
+    this.exportColumns = this.cols.map(( col ) => ({ header: col.field, dataKey: col.header }));
+
+    this.listItemsDownload();
   }
 
+  private listItemsDownload() {
+    this.itemsDownload = [
+      {
+        label: 'Pdf',
+        icon: 'pi pi-file-pdf',
+        command: () => {
+          this._downloadDocumentService
+            .exportPdf(this.exportColumns, this.data);
+        }
+      },
+      {
+        label: 'Excel',
+        icon: 'pi pi-file-excel',
+        command: () => { }
+      },
+    ];
+  }
 }
+
 const DATA = [
   { "fecha": "10/20/50", "monto": "10/20/50", "placa": "454SD", "empresa": "Test", "ruta": "Lima", "ubicacion": "Europa", "origen": "test" },
   { "fecha": "10/20/50", "monto": "10/20/50", "placa": "454SD", "empresa": "Test", "ruta": "Lima", "ubicacion": "Europa", "origen": "test" },
