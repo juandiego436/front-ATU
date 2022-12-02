4031cd1a3c20b4a9dc346502f25d5a2f5a6d8fe8
Gamaliel Abanto <gabanto@canvia.com>
2022-12-02 11:23:36 -0500

    page qr, control tarifario

--- src/app/pages/control-tarifario/control-tarifario.component.ts
+++ src/app/pages/control-tarifario/control-tarifario.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ExportService } from '@helpers/export.service';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-control-tarifario',
@@ -6,5 +8,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./control-tarifario.component.scss']
 })
 export class ControlTarifarioComponent {
+  data: any[] = DATA;
+  first = 0;
+  date: Date;
+  itemsDownload: MenuItem[];
+  cols: any[] = [
+    { field: 'Fecha', header: 'fecha' },
+    { field: 'Monto', header: 'monto' },
+    { field: 'Placa', header: 'placa' },
+    { field: 'Empresa', header: 'fecha' },
+    { field: 'Ruta', header: 'ruta' },
+    { field: 'Ubicacion', header: 'ubicacion' },
+    { field: 'Origen', header: 'origen' }
+  ];
+  exportColumns: any[];
+  constructor(
+    private _exportService: ExportService
+  ) { }
 
+  ngOnInit(): void {
+    this.exportColumns = this.cols.map(( col ) => ({ header: col.field, dataKey: col.header }));
+
+    this.listItemsDownload();
+  }
+
+  private listItemsDownload() {
+    this.itemsDownload = [
+      {
+        label: 'Pdf',
+        icon: 'pi pi-file-pdf',
+        command: () => {
+          this._exportService
+            .exportPdf(this.exportColumns, this.data);
+        }
+      },
+      {
+        label: 'Excel',
+        icon: 'pi pi-file-excel',
+        command: () => { }
+      },
+    ];
+  }
 }
+
+const DATA = [
+  { "fecha": "10/20/50", "monto": "10/20/50", "placa": "454SD", "empresa": "Test", "ruta": "Lima", "ubicacion": "Europa", "origen": "test" },
+  { "fecha": "10/20/50", "monto": "10/20/50", "placa": "454SD", "empresa": "Test", "ruta": "Lima", "ubicacion": "Europa", "origen": "test" },
+  { "fecha": "10/20/50", "monto": "10/20/50", "placa": "454SD", "empresa": "Test", "ruta": "Lima", "ubicacion": "Europa", "origen": "test" },
+  { "fecha": "10/20/50", "monto": "10/20/50", "placa": "454SD", "empresa": "Test", "ruta": "Lima", "ubicacion": "Europa", "origen": "test" },
+  { "fecha": "10/20/50", "monto": "10/20/50", "placa": "454SD", "empresa": "Test", "ruta": "Lima", "ubicacion": "Europa", "origen": "test" },
+  { "fecha": "10/20/50", "monto": "10/20/50", "placa": "454SD", "empresa": "Test", "ruta": "Lima", "ubicacion": "Europa", "origen": "test" },
+  { "fecha": "10/20/50", "monto": "10/20/50", "placa": "454SD", "empresa": "Test", "ruta": "Lima", "ubicacion": "Europa", "origen": "test" },
+  { "fecha": "10/20/50", "monto": "10/20/50", "placa": "454SD", "empresa": "Test", "ruta": "Lima", "ubicacion": "Europa", "origen": "test" },
+  { "fecha": "10/20/50", "monto": "10/20/50", "placa": "454SD", "empresa": "Test", "ruta": "Lima", "ubicacion": "Europa", "origen": "test" },
+]
--- src/app/pages/generador-qr/generador-qr.component.ts
+++ src/app/pages/generador-qr/generador-qr.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./generador-qr.component.scss']
 })
 export class GeneradorQrComponent {
+  data: any[] = DATA;
+  first = 0;
+  cols: any[] = [
+    { field: 'Empresa de Transporte', header: 'empresa' },
+    { field: 'Ruc', header: 'ruc' },
+    { field: 'Placa de registro', header: 'placa' },
+    { field: 'Código de ruta', header: 'codRuta' },
+    { field: 'Tipo de unidad', header: 'tipoUnidad' },
+    { field: 'Número de asientos', header: 'numeroAsientos' }
+  ];
+  
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 
 }
+
+const DATA = [
+  { empresa: "Software SOF", ruc: "102085640540", placa: "454SD", codRuta: "ASDS45SD", tipoUnidad: "Test", numeroAsientos: 10 },
+  { empresa: "Software SOF", ruc: "102085640540", placa: "454SD", codRuta: "ASDS45SD", tipoUnidad: "Test", numeroAsientos: 10 },
+  { empresa: "Software SOF", ruc: "102085640540", placa: "454SD", codRuta: "ASDS45SD", tipoUnidad: "Test", numeroAsientos: 10 },
+  { empresa: "Software SOF", ruc: "102085640540", placa: "454SD", codRuta: "ASDS45SD", tipoUnidad: "Test", numeroAsientos: 10 },
+  { empresa: "Software SOF", ruc: "102085640540", placa: "454SD", codRuta: "ASDS45SD", tipoUnidad: "Test", numeroAsientos: 10 },
+  { empresa: "Software SOF", ruc: "102085640540", placa: "454SD", codRuta: "ASDS45SD", tipoUnidad: "Test", numeroAsientos: 10 },
+  { empresa: "Software SOF", ruc: "102085640540", placa: "454SD", codRuta: "ASDS45SD", tipoUnidad: "Test", numeroAsientos: 10 },
+  { empresa: "Software SOF", ruc: "102085640540", placa: "454SD", codRuta: "ASDS45SD", tipoUnidad: "Test", numeroAsientos: 10 },
+  { empresa: "Software SOF", ruc: "102085640540", placa: "454SD", codRuta: "ASDS45SD", tipoUnidad: "Test", numeroAsientos: 10 },
+  { empresa: "Software SOF", ruc: "102085640540", placa: "454SD", codRuta: "ASDS45SD", tipoUnidad: "Test", numeroAsientos: 10 },
+  { empresa: "Software SOF", ruc: "102085640540", placa: "454SD", codRuta: "ASDS45SD", tipoUnidad: "Test", numeroAsientos: 10 },
+  { empresa: "Software SOF", ruc: "102085640540", placa: "454SD", codRuta: "ASDS45SD", tipoUnidad: "Test", numeroAsientos: 10 },
+  { empresa: "Software SOF", ruc: "102085640540", placa: "454SD", codRuta: "ASDS45SD", tipoUnidad: "Test", numeroAsientos: 10 },
+  { empresa: "Software SOF", ruc: "102085640540", placa: "454SD", codRuta: "ASDS45SD", tipoUnidad: "Test", numeroAsientos: 10 }
+]
